ae6522608eeb70ad1642a5ed664c515b73236705
SrinuK <srinuk@thoughtworks.com>
2018-06-17 22:56:18 +0530

    Corrected the assertion statement and removed the unnecessary files

--- src/test/java/stepDefs/LoginPageSteps.java
+++ src/test/java/stepDefs/LoginPageSteps.java
@@ -3,10 +3,12 @@ package stepDefs;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import framework.pages.LoginPage;
+import org.junit.Assert;
 
 public class LoginPageSteps {
     @Then("^User should see page title as \"([^\"]*)\"$")
     public void userShouldSeePageTitleAs(String validationText) throws Throwable {
-        new LoginPage().getTitle().equals(validationText);
+        Assert.assertEquals(new LoginPage().getTitle(), validationText);
+
     }
 }
