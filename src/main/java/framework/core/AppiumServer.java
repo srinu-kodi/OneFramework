b82ec635f3e32bcafdfabce493d3802ae7123254
SrinuK <srinuk@thoughtworks.com>
2018-10-13 13:26:47 +0530

    Fixed the AppiumServer issue which was not getting started with latest version of Appium. From now we need to metion Appium JS file to AppiumDriverLocaleServce inorder to start appium

--- src/main/java/framework/core/AppiumServer.java
+++ src/main/java/framework/core/AppiumServer.java
@@ -1,10 +1,10 @@
 package framework.core;
 
-import framework.exceptionManager.AppiumException;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 
+import java.io.File;
 import java.io.IOException;
 
 public class AppiumServer {
@@ -12,32 +12,17 @@ public class AppiumServer {
     static AppiumDriverLocalService appium;
 
     public static void start() throws IOException {
-        try {
-            AppiumServiceBuilder builder = new AppiumServiceBuilder().withArgument(GeneralServerFlag.SESSION_OVERRIDE);
-            appium = builder.build();
-            appium.start();
-            if (appium != null) {
-                System.out.println("Appium server is started now");
-            } else {
-                throw new AppiumException();
-            }
-        } catch (AppiumException ex) {
-            ex.printStackTrace();
-        }
+        AppiumServiceBuilder builder = new AppiumServiceBuilder()
+                .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+                .withArgument(GeneralServerFlag.SESSION_OVERRIDE)
+                .withLogFile(new File(System.getProperty("user.dir") + "/appiumServerLog.txt"));
+        appium = builder.build();
+        System.out.println("Starting the Appium Server on 127.0.0.1:4723");
+        appium.start();
     }
 
-    public static void stop() throws IOException {
-        try {
-            if (appium == null) {
-                throw new AppiumException();
-            }
-            else {
-                appium.stop();
-                System.out.println("Appium server is stopped now");
-            }
-        } catch (AppiumException ex) {
-            ex.printStackTrace();
-        }
-
+    public static void stop() {
+        System.out.println("Stopping the Appium Server");
+        appium.stop();
     }
 }
