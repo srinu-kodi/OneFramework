5e5c4d5052d92ff6d0f96a1692ebddbf81ce8736
SrinuK <srinuk@thoughtworks.com>
2019-01-27 23:32:53 +0530

    Refactoring in Image Comparator class. Creating cleaner methods for Image Comparison. - Earlier implementation was capture & compare and they are taing lot of responsibilities. - With new changes, it is clean segregation of responsibilities.

--- src/main/java/org/oneframework/imageCompare/ImageComparator.java
+++ src/main/java/org/oneframework/imageCompare/ImageComparator.java
@@ -20,14 +20,29 @@ import static org.oneframework.logger.LoggingManager.logMessage;
 public class ImageComparator extends DeviceConfig {
     WebDriver driver;
     public static boolean COMPARE = false;
-    String baselineImageDirFullPath = "baselineImages/" + getPlatformModelName() + "/";
+    String baselineImageDirFullPath;
 
     public ImageComparator(WebDriver driver) {
         this.driver = driver;
+        baselineImageDirFullPath = "baselineImages/" + getPlatformModelName() + "/";
     }
 
-    public void capture(String imageName) throws IOException {
-        File imageFile = new File(imageName + ".png");
+    private BufferedImage getBaselineImage(String imageName) throws IOException {
+        File expectedImageFile = FileUtils.getFile(FileUtility.getFile(baselineImageDirFullPath + imageName + ".png").getAbsolutePath());
+        BufferedImage expectedImage = ImageIO.read(expectedImageFile);
+        ImageIO.write(expectedImage, "png", expectedImageFile);
+        logMessage("Retrieving the baseline image " + imageName + " from " + baselineImageDirFullPath);
+        return expectedImage;
+    }
+
+    private BufferedImage captureActualImage(String imageName) throws IOException {
+        File imageFile;
+        if (ImageComparator.COMPARE) {
+            imageFile = new File(imageName + "_actual.png");
+        } else {
+            imageFile = new File(imageName + ".png");
+        }
+
         BufferedImage image = new AShot().takeScreenshot(driver).getImage();
         if (driver instanceof IOSDriver) {
             image = image.getSubimage(0, 48, 740, 1280);
@@ -36,42 +51,34 @@ public class ImageComparator extends DeviceConfig {
         }
         ImageIO.write(image, "png", imageFile);
         FileUtility.copyFileToDirectory(imageFile, new File(baselineImageDirFullPath));
-        logMessage("Capturing the baseline image " + imageName + " into local baselineImages directory " + baselineImageDirFullPath);
+        logMessage("Capturing the image " + imageName + " into local baselineImages directory " + baselineImageDirFullPath);
         FileUtility.forceDelete(imageFile);
+        return image;
+    }
+
+    public void createDiffImageAs(ImageDiff diffImg, String imageName) throws IOException {
+        BufferedImage diffImage = diffImg.getMarkedImage();
+        File diffImageFile = new File(imageName + "_diffImage.png");
+        ImageIO.write(diffImage, "png", diffImageFile);
+        FileUtility.copyFileToDirectory(diffImageFile, new File(baselineImageDirFullPath));
+        logMessage("Saving the difference image into " + baselineImageDirFullPath);
+        FileUtility.forceDelete(diffImageFile);
     }
 
     public boolean compare(String imageName) throws IOException {
+        BufferedImage actualImage, expectedImage;
+        ImageDiff diffImage;
         boolean imageMatchFlag = false;
         if (!ImageComparator.COMPARE) {
-            capture(imageName);
+            captureActualImage(imageName);
             imageMatchFlag = true;
         } else {
-            File actualImageFile = new File(imageName + "_actual.png");
-            BufferedImage actualImage = new AShot().takeScreenshot(driver).getImage();
-            if (driver instanceof IOSDriver) {
-                actualImage = actualImage.getSubimage(0, 48, 740, 1280);
-            } else if (driver instanceof AndroidDriver) {
-                actualImage = actualImage.getSubimage(0, 64, 1080, 1728);
-            }
-            ImageIO.write(actualImage, "png", actualImageFile);
-            FileUtility.copyFileToDirectory(actualImageFile, new File(baselineImageDirFullPath));
-            logMessage("Capturing the actual image " + imageName + " into local baselineImages directory " + baselineImageDirFullPath);
-            FileUtility.forceDelete(actualImageFile);
-
-            File expectedImageFile = FileUtils.getFile(FileUtility.getFile(baselineImageDirFullPath + imageName + ".png").getAbsolutePath());
-            BufferedImage expectedImage = ImageIO.read(expectedImageFile);
-            ImageIO.write(expectedImage, "png", expectedImageFile);
-            logMessage("Reading the baseline image " + imageName + " from " + baselineImageDirFullPath);
-            ImageDiff diff = new ImageDiffer().makeDiff(expectedImage, actualImage);
+            expectedImage = getBaselineImage(imageName);
+            actualImage = captureActualImage(imageName);
+            diffImage = new ImageDiffer().makeDiff(expectedImage, actualImage);
             logMessage("Comparing the expected baseline image with actual image");
-
-            if (diff.getDiffSize() > 0) {
-                BufferedImage diffImage = diff.getMarkedImage();
-                File diffImageFile = new File(imageName + "_diffImage.png");
-                ImageIO.write(diffImage, "png", diffImageFile);
-                FileUtility.copyFileToDirectory(diffImageFile, new File(baselineImageDirFullPath));
-                logMessage("Saving the difference image into " + baselineImageDirFullPath);
-                FileUtility.forceDelete(diffImageFile);
+            if (diffImage.getDiffSize() > 0) {
+                createDiffImageAs(diffImage, imageName);
             } else {
                 imageMatchFlag = true;
             }
