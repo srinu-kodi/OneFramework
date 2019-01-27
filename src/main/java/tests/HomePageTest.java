803db32eb1b12cc8a0544b0708d4fc63598a7f09
SrinuK <srinuk@thoughtworks.com>
2019-01-27 23:59:18 +0530

    Introduing the Soft Assert to tests. - As Hard Assert does not proceed further if there is assertion failure; With soft assertion, test will fail at the end of the test execution if there are assertion failures.

--- src/main/java/tests/HomePageTest.java
+++ src/main/java/tests/HomePageTest.java
@@ -3,26 +3,30 @@ package tests;
 import org.oneframework.allureReport.TestListener;
 import org.oneframework.imageCompare.ImageComparator;
 import org.oneframework.pageObjects.HomePage;
-import org.testng.*;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 @Listeners({TestListener.class})
 public class HomePageTest extends BaseTest {
 
+    SoftAssert softAssert = new SoftAssert();
+
     @Test(description = "testing the signin page visually")
     public void testSignInOption() throws Exception {
         HomePage homePage = new HomePage(driver);
-        Assert.assertTrue(new ImageComparator(driver).compare("homePage"), "homePage baseline image isn't matching with actual image.");
+        softAssert.assertTrue(new ImageComparator(driver).compare("homePage"), "homePage baseline image isn't matching with actual image.");
         homePage.chooseSignInOption();
-        Assert.assertTrue(new ImageComparator(driver).compare("signinPage"), "signinPage baseline image isn't matching with actual image.");
+        softAssert.assertTrue(new ImageComparator(driver).compare("signinPage"), "signinPage baseline image isn't matching with actual image.");
+        softAssert.assertAll();
     }
 
     @Test(description = "testing the signup page visually")
     public void testSignUpOption() throws Exception {
         HomePage homePage = new HomePage(driver);
         homePage.chooseSignUpOption();
-        Assert.assertTrue(new ImageComparator(driver).compare("signupPage"), "signupPage baseline image isn't matching with actual image.");
+        softAssert.assertTrue(new ImageComparator(driver).compare("signupPage"), "signupPage baseline image isn't matching with actual image.");
+        softAssert.assertAll();
     }
 
 }
